59a2e7534bdddd154d968176ddcd5e632b4e074e
Joseph Chiocchi <input.output@gmail.com>
2023-11-14 01:02:18 +0000

    init check_push_to_github

---
+++ check_push_to_github/__main__.py
@@ -0,0 +1,45 @@
+import json
+import os
+import urllib.request
+from datetime import datetime
+
+
+github_event_url = "https://api.github.com/users/yyolk/events"
+headers = {
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28",
+}
+taskade_reoccurring_push_to_github_task_id = os.environ[
+    "taskade_reoccurring_push_to_github_task_id"
+]
+taskade_reoccurring_project_id = os.environ["taskade_reoccurring_project_id"]
+taskade_api_pat = os.environ["taskade_api_pat"]
+request = urllib.request.Request(github_event_url, headers=headers)
+response = urllib.request.urlopen(request)
+data = json.load(response)
+events_interested_in_leetcode_dailies_only = list(
+    filter(
+        lambda x: x["type"] == "PushEvent"
+        and "repo" in x
+        and x["repo"]["name"] == "yyolk/leetcode_dailies",
+        data,
+    )
+)
+push_events = list(filter(lambda x: x["type"] == "PushEvent", data))
+for event in push_events:
+    if (
+        dtelta := (
+            datetime.now()
+            - datetime.strptime(event["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+        )
+    ).days == 0:
+        print(True)
+        url_task_complete = f"https://www.taskade.com/api/v1/projects/{taskade_reoccurring_project_id}/tasks/{taskade_reoccurring_push_to_github_task_id}/complete"
+        headers = {"Authorization": f"Bearer {taskade_api_pat}"}
+        request = urllib.request.Request(url_task_complete, headers=headers)
+        response = urllib.request.urlopen(request)
+        print(response.code)
+        break
+    print(datetime.now() - datetime.strptime(event["created_at"], "%Y-%m-%dT%H:%M:%SZ"))
+else:
+    print(False, "no push events today")
