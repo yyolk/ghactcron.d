573643baf15a9e49a3a3927689a6548b84f8f9e4
Joseph Chiocchi <input.output@gmail.com>
2023-11-14 01:54:36 +0000

    rough in leetcode_dailies daily habit

--- check_push_to_github/__main__.py
+++ check_push_to_github/__main__.py
@@ -12,19 +12,15 @@ headers = {
 taskade_reoccurring_push_to_github_task_id = os.environ[
     "taskade_reoccurring_push_to_github_task_id"
 ]
+taskade_reoccurring_leetcode_daily_task_id = os.environ[
+    "taskade_reoccurring_leetcode_daily_task_id"
+]
 taskade_reoccurring_project_id = os.environ["taskade_reoccurring_project_id"]
 taskade_api_pat = os.environ["taskade_api_pat"]
 request = urllib.request.Request(github_event_url, headers=headers)
 with urllib.request.urlopen(request) as response:
     data = json.load(response)
-events_interested_in_leetcode_dailies_only = list(
-    filter(
-        lambda x: x["type"] == "PushEvent"
-        and "repo" in x
-        and x["repo"]["name"] == "yyolk/leetcode_dailies",
-        data,
-    )
-)
+
 push_events = list(filter(lambda x: x["type"] == "PushEvent", data))
 for event in push_events:
     if (
@@ -36,11 +32,41 @@ for event in push_events:
         print(True)
         url_task_complete = f"https://www.taskade.com/api/v1/projects/{taskade_reoccurring_project_id}/tasks/{taskade_reoccurring_push_to_github_task_id}/complete"
         headers = {"Authorization": f"Bearer {taskade_api_pat}"}
-        request = urllib.request.Request(url_task_complete, headers=headers, method="POST")
+        request = urllib.request.Request(
+            url_task_complete, headers=headers, method="POST"
+        )
         with urllib.request.urlopen(request) as response:
             print(response.code)
         break
     print(datetime.now() - datetime.strptime(event["created_at"], "%Y-%m-%dT%H:%M:%SZ"))
-
 else:
     print(False, "no push events today")
+
+events_interested_in_leetcode_dailies_only = list(
+    filter(
+        lambda x: x["type"] == "PushEvent"
+        and "repo" in x
+        and x["repo"]["name"] == "yyolk/leetcode_dailies",
+        data,
+    )
+)
+# This needs some thought
+# for leetcode_daily_event in events_interested_in_leetcode_dailies_only:
+#     if (
+#         dtelta := (
+#             datetime.now()
+#             - datetime.strptime(event["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+#         )
+#     ).days == 0:
+#         print(True)
+#         url_task_complete = f"https://www.taskade.com/api/v1/projects/{taskade_reoccurring_project_id}/tasks/{taskade_reoccurring_leetcode_daily_task_id}/complete"
+#         headers = {"Authorization": f"Bearer {taskade_api_pat}"}
+#         request = urllib.request.Request(
+#             url_task_complete, headers=headers, method="POST"
+#         )
+#         with urllib.request.urlopen(request) as response:
+#             print(response.code)
+#         break
+
+# else:
+#     print(False, "no leetcode_dailies push events today")
